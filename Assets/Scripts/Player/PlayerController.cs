8aa4dfe75af41008d6ad370c1303eeaa0442b3ed
Dexler <69513582+DexlerXD@users.noreply.github.com>
2024-11-04 02:06:17 +0300

    Added simple player movement controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,10 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using VContainer;
+using UnityEngine.InputSystem;
 
 namespace Secrets.Player
 {
     public sealed class PlayerController : MonoBehaviour
     {
+        [SerializeField] private float _moveSpeed = 1f;
         //[Inject] private readonly FooService _fooService;
+        
+        private InputAction _moveAction; 
+        
+        private void Start()
+        {
+            // Get movement actions from InputSystem
+            _moveAction = InputSystem.actions.FindAction("Move");
+        }
+
+        private void Update()
+        {
+            // Get movement vector and apply it to the player
+            var moveVector = _moveAction.ReadValue<Vector2>().normalized * (Time.deltaTime * _moveSpeed);
+            transform.Translate(moveVector);
+        }
     }
+    
 }
